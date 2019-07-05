e552a31e844a19aa3ddfdb43d19ef7c9eeeacd4c
jungwoo <plum.jungwoo@gmail.com>
2019-07-05 21:08:24 +0900

    streaming test 2 !!!

--- lazy_test1/views.py
+++ lazy_test1/views.py
@@ -71,9 +71,12 @@ cam = VideoCamera()
 
 def gen(camera):
     while True:
-        frame = cam.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        try:
+            frame = cam.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        except :
+            pass
 
 
 # @gzip.gzip_page
